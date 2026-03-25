5d7ffc8c5387c28ad3f0daf9f2af56a2482ad9a8
autosvillarosa <autosvillarosa.app@gmail.com>
2026-03-25 02:09:02 +0100

    PRUEBA 100

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "05bc03b93856c66c97e590df80b0b290
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "38ad388f11fecb0be4bd1e3b31ae1cfb",
+"flutter_bootstrap.js": "55315803c6abbbfadd54743045357aec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
